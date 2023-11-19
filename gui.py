93b83b008fa3553319a3ccf761de2d0d88ca7427
Truan Leiker <tleiker20@ku.edu>
2023-11-19 12:53:22 -0600

    Imported image is displayed in GUI

--- gui.py
+++ gui.py
@@ -1,14 +1,28 @@
 from tkinter import *
 from tkinter import filedialog
+from PIL import Image, ImageTk
 
 # Opens file explorer to facilitate selection of image file to be loaded in program
 def open_image():
 
-    filename = filedialog.askopenfilename(initialdir = "/", title = "Select an Image",
+    filename = filedialog.askopenfilename(initialdir = ".", title = "Select an Image",
                                           filetypes = (("Image files", "*.png* *.jpg"), 
                                                        ("all files", "*.*")))
     
-    image_label.config(text=filename)
+    # Open image
+    image = Image.open(filename)
+
+    # Resize image
+    base_height = 300
+    hpercent = (base_height / float(image.size[1]))
+    wsize = int((float(image.size[0]) * float(hpercent)))
+    image = image.resize((wsize, base_height), Image.Resampling.LANCZOS)
+
+    # Display image
+    tkimage = ImageTk.PhotoImage(image)
+    image_label.config(image=tkimage)
+    image_label.config(height=base_height)
+    image_label.image = tkimage
 
 # Opens dialog box containing information on the program
 def open_about_dialog():
@@ -40,14 +54,16 @@ help_menu.add_command(label='About', command=open_about_dialog)
 take_photo_button = Button(window, text='Take Photo')
 import_image_button = Button(window, text='Import Image', command=open_image)
 view_results_button = Button(window, text='View results')
-image_label = Label(window, text='Imported image will display here', borderwidth=1, relief='solid', width=50, height = 15)
+image_frame = Frame(window, width="1000", height="1000", borderwidth=1, relief='solid')
+image_label = Label(image_frame, text='Imported image will display here', height=20)
+image_label.pack(fill=BOTH, expand=True)
 convert_button = Button(window, text='Convert')
 
 # Arrange widgets
 take_photo_button.grid(column=0, row=0, sticky='nesw', padx=4, pady=4)
 import_image_button.grid(column=1, row=0, sticky='nesw', padx=4, pady=4)
 view_results_button.grid(column=2, row=0, sticky='nesw', padx=4, pady=4)
-image_label.grid(column=0, row=1, columnspan=3, padx=4, pady=4)
+image_frame.grid(column=0, row=1, sticky='nesw', columnspan=3, padx=4, pady=4)
 convert_button.grid(column=0, row=2, sticky='nesw', padx=4, pady=4)
 
 # Begin main loop
